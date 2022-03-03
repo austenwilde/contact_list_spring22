2361f018e966d0f375fece1bffa188bb3211749d
Austen Wilde <sawilde15@gmail.com>
2022-03-02 20:47:43 -0700

    end of models

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :contact
   has_many :comments, dependent: :destroy
+
+  validates :body, presence: true
+  validates :body, length: { in: 10...500 }
 end
 
